0bfe41a4aa1ba14edf0f9722acc8f1b866227db5
Dave.o Olund <dave.o@crowdbotics.com>
2022-12-05 11:24:48 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,10 +1,11 @@
+import { Text } from "react-native";
 import { TextInput } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled2 = () => {
   return <View style={_styles.JFdnnDJC}>
-      <TextInput style={_styles.zZglSGFB}></TextInput></View>;
+      <Text style={_styles.EmuXhvvU}>Customer</Text><TextInput style={_styles.prfmHMce}></TextInput><Text style={_styles.sJYWVeic}>Customer</Text><TextInput style={_styles.fwmPegIJ}></TextInput></View>;
 };
 
 export default Untitled2;
@@ -17,6 +18,43 @@ const _styles = StyleSheet.create({
     height: "100%"
   },
   zZglSGFB: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  EmuXhvvU: {
+    width: 100,
+    height: 18,
+    lineHeight: 14,
+    fontSize: 16,
+    borderRadius: 0,
+    fontWeight: "700"
+  },
+  prfmHMce: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 20,
+    alignItems: "start",
+    position: "relative"
+  },
+  CqUShmYD: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  sJYWVeic: {
+    width: 100,
+    height: 18,
+    lineHeight: 14,
+    fontSize: 16,
+    borderRadius: 0,
+    fontWeight: "700"
+  },
+  fwmPegIJ: {
     backgroundColor: "#ffffff",
     borderColor: "#cccccc",
     width: 150,
