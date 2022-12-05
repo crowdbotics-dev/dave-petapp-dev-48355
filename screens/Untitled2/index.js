eb1a2c23b9595ed766682c8fa74726891e511e47
Dave.o Olund <dave.o@crowdbotics.com>
2022-12-05 11:15:25 -0500

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,25 @@
+import { TextInput } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled2 = () => {
+  return <View style={_styles.JFdnnDJC}>
+      <TextInput style={_styles.zZglSGFB}></TextInput></View>;
+};
+
+export default Untitled2;
+
+const _styles = StyleSheet.create({
+  JFdnnDJC: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  zZglSGFB: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});
